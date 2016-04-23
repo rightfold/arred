d8d49cd40e0728d611d689e3c830574fd692af6c
rightfold <rightfold@gmail.com>
2016-04-23 18:31:24 +0200

    Use AST instead of DAG

--- src/ts/Arred/CanvasView.tsx
+++ src/ts/Arred/CanvasView.tsx
@@ -7,90 +7,31 @@ namespace Arred {
 
     interface State { }
 
-    const nodes = Map<string, Computation>([
-        ['a', identityComputation],
-        ['b', new CustomComputation('map square')],
-        ['c', new CustomComputation('map cube')],
-        ['d', new CustomComputation('map pow4')],
-        ['e', new CustomComputation('uncurry zip')],
-        ['f', voidComputation],
-    ]);
-
-    const connections = OrderedMap<string, Set<string>>([
-        ['a', Set<string>(['b', 'c', 'd'])],
-        ['b', Set<string>(['e'])],
-        ['c', Set<string>(['e'])],
-        ['d', Set<string>(['e'])],
-        ['e', Set<string>(['f'])],
-        ['f', Set<string>([])],
-    ]);
-
-    interface Pos { x: number; y: number; }
-    let positions = Map<string, Pos>();
-    function setPositions(id: string, x: number, y: number): void {
-        if (!positions.has(id)) {
-            positions = positions.set(id, {
-                x: 64 + x * 256,
-                y: 64 + y * 128,
-            });
-        }
-        connections.get(id).toList().forEach((id, i) => setPositions(id, x + 1, y + i));
-    }
-    setPositions('a', 0, 0);
+    const computation =
+        new PlusComputation(
+            zeroArrowComputation,
+            new PlusComputation(
+                new CompositionComputation(
+                    voidArrowComputation,
+                    zeroArrowComputation
+                ),
+                voidArrowComputation
+            )
+        );
 
     export class CanvasView extends React.Component<Props, State> {
         render(): JSX.Element {
             return <div className='arred-canvas'>
-                <svg xmlns="http://www.w3.org/2000/svg">
-                    {nodes.entrySeq().map(([id, computation], i) =>
-                        <g
-                            key={id}
+                <svg xmlns='http://www.w3.org/2000/svg'>
+                    <g
+                        fill='transparent'
+                        stroke='black'
+                        strokeWidth={2}
 
-                            fill='transparent'
-                            stroke='black'
-                            strokeWidth={2}
-                        >
-                            <g transform={'translate(' + positions.get(id).x + ', ' + positions.get(id).y + ')'}>
-                                {computation.svg}
-                                <line
-                                    x1={-32}
-                                    y1={32}
-                                    x2={0}
-                                    y2={32}
-                                />
-                                <line
-                                    x1={-8}
-                                    y1={32 - 8}
-                                    x2={0}
-                                    y2={32}
-                                />
-                                <line
-                                    x1={-8}
-                                    y1={32 + 8}
-                                    x2={0}
-                                    y2={32}
-                                />
-                                <line
-                                    x1={128}
-                                    y1={32}
-                                    x2={128 + 32}
-                                    y2={32}
-                                />
-                            </g>
-                            <g>
-                                {connections.get(id).toList().map(targetId =>
-                                    <line
-                                        key={targetId}
-
-                                        x1={positions.get(id).x + 128 + 32}
-                                        y1={positions.get(id).y + 32}
-                                        x2={positions.get(targetId).x - 32}
-                                        y2={positions.get(targetId).y + 32}
-                                    />
-                                )}
-                            </g>
-                        </g>
-                    )}
+                        transform='translate(32, 32)'
+                    >
+                        {whiteBoxSVG(computation, 0, 0)}
+                    </g>
                 </svg>
             </div>;
         }
--- src/ts/Arred/Computation.tsx
+++ src/ts/Arred/Computation.tsx
@@ -1,43 +1,167 @@
 namespace Arred {
+    const unitWidth = 128;
+    const unitHeight = 64;
+
     export interface Computation {
+        haskell: string;
         svg: JSX.Element;
+        width: number;
+        height: number;
     }
 
-    export const identityComputation = {
-        svg: <line x1={0} y1={64 / 2} x2={128} y2={64 / 2} />,
-    };
+    export function whiteBoxSVG(c: Computation, x: number, y: number): JSX.Element {
+        const absX = x * unitWidth;
+        const absY = y * unitHeight;
+        return <g transform={'translate(' + absX + ',' + absY + ')'}>
+            <title>{c.haskell}</title>
+            {c.svg}
+        </g>;
+    }
 
-    export const voidComputation = {
+    export const voidArrowComputation: Computation = {
+        haskell: 'arr $ const ()',
         svg: <g>
-            <line x1={0} y1={64 / 2} x2={128} y2={64 / 2} />
+            <line
+                x1={0}
+                y1={unitHeight / 2}
+                x2={unitWidth / 2 - 16}
+                y2={unitHeight / 2}
+            />
+            <line
+                x1={unitWidth / 2 + 16}
+                y1={unitHeight / 2}
+                x2={unitWidth}
+                y2={unitHeight / 2}
+            />
             <rect
-                x={128 / 2 - 16}
-                y={64 / 2 - 16}
+                x={unitWidth / 2 - 16}
+                y={unitHeight / 2 - 16}
                 width={32}
                 height={32}
-                fill="black"
+                fill='black'
             />
         </g>,
+        width: 1,
+        height: 1,
     };
 
-    export class CustomComputation implements Computation {
-        constructor(private source: string) { }
+    export const zeroArrowComputation: Computation = {
+        haskell: 'zeroArrow',
+        svg: <g>
+            <line
+                x1={0}
+                y1={unitHeight / 2}
+                x2={unitWidth / 2 - 16}
+                y2={unitHeight / 2}
+            />
+            <line
+                x1={unitWidth / 2 + 16}
+                y1={unitHeight / 2}
+                x2={unitWidth}
+                y2={unitHeight / 2}
+            />
+            <circle
+                cx={unitWidth / 2}
+                cy={unitHeight / 2}
+                r={16}
+            />
+        </g>,
+        width: 1,
+        height: 1,
+    };
+
+    export class CompositionComputation implements Computation {
+        constructor(private from: Computation, private to: Computation) { }
+
+        get haskell(): string {
+            return '(' + this.from.haskell + ') >>> (' + this.to.haskell + ')';
+        }
 
         get svg(): JSX.Element {
             return <g>
-                <rect x={0} y={0} width={128} height={64} />
-                <text
-                    x={8}
-                    y={21}
-
-                    fontFamily="Consolas"
-
-                    fill="black"
-                    strokeWidth="0"
-                >
-                    {this.source}
-                </text>
+                {whiteBoxSVG(this.from, 0, 0)}
+                {whiteBoxSVG(this.to, this.from.width, 0)}
             </g>;
+        }
+
+        get width(): number {
+            return this.from.width + this.to.width;
+        }
+
+        get height(): number {
+            return Math.max(this.from.height, this.to.height);
+        }
+    }
+
+    export class PlusComputation implements Computation {
+        constructor(private left: Computation, private right: Computation) { }
+
+        get haskell(): string {
+            return '(' + this.left.haskell + ') <+> (' + this.right.haskell + ')';
+        }
+
+        get svg(): JSX.Element {
+            return <g>
+                {whiteBoxSVG(
+                    this.left,
+                    this.width - 1 - this.left.width,
+                    0
+                )}
+                <line
+                    x1={(this.width - 1) * unitWidth}
+                    y1={this.left.height * unitHeight / 2}
+                    x2={this.width * unitWidth - unitWidth / 2 - 16 - 16}
+                    y2={this.left.height * unitHeight + unitHeight / 2}
+                />
+                <line
+                    x1={(this.width - 1) * unitWidth}
+                    y1={(this.left.height + 1) * unitHeight + this.right.height * unitHeight / 2}
+                    x2={this.width * unitWidth - unitWidth / 2 - 16 - 16}
+                    y2={this.left.height * unitHeight + unitHeight / 2}
+                />
+                <line
+                    x1={this.width * unitWidth - unitWidth / 2 - 16 - 16}
+                    y1={this.left.height * unitHeight + unitHeight / 2}
+                    x2={this.width * unitWidth - unitWidth / 2 - 16}
+                    y2={this.left.height * unitHeight + unitHeight / 2}
+                />
+                <circle
+                    cx={this.width * unitWidth - unitWidth / 2}
+                    cy={this.left.height * unitHeight + unitHeight / 2}
+                    r={16}
+                />
+                <line
+                    x1={this.width * unitWidth - unitWidth / 2 - 8}
+                    y1={this.left.height * unitHeight + unitHeight / 2}
+                    x2={this.width * unitWidth - unitWidth / 2 + 8}
+                    y2={this.left.height * unitHeight + unitHeight / 2}
+                />
+                <line
+                    x1={this.width * unitWidth - unitWidth / 2}
+                    y1={this.left.height * unitHeight + unitHeight / 2 - 8}
+                    x2={this.width * unitWidth - unitWidth / 2}
+                    y2={this.left.height * unitHeight + unitHeight / 2 + 8}
+                />
+                <line
+                    x1={this.width * unitWidth - unitWidth / 2 + 16}
+                    y1={this.left.height * unitHeight + unitHeight / 2}
+                    x2={this.width * unitWidth}
+                    y2={this.left.height * unitHeight + unitHeight / 2}
+                />
+                {whiteBoxSVG(
+                    this.right,
+                    this.width - 1 - this.right.width,
+                    this.left.height + 1
+                )}
+            </g>;
+        }
+
+        get width(): number {
+            return Math.max(this.left.width, this.right.width) + 1;
+        }
+
+        get height(): number {
+            return this.left.height + 1 + this.right.height;
         }
     }
 }
